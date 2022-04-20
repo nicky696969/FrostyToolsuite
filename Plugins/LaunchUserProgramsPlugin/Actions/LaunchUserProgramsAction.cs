7e175d25649d7aeb0e72de4ba14e7e4c01d79fae
Dyvine57 <dulana57@gmail.com>
2022-04-20 15:14:37 -0400

    launch user programs plugin

---
+++ Plugins/LaunchUserProgramsPlugin/Actions/LaunchUserProgramsAction.cs
@@ -0,0 +1,126 @@
+﻿using Frosty.Core;
+using Frosty.ModSupport;
+using FrostySdk;
+using FrostySdk.Interfaces;
+using LaunchUserProgramsPlugin.Options;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace LaunchUserProgramsPlugin.Actions
+{
+    public class LaunchUserProgramsAction : ExecutionAction
+    {
+        public override Action<ILogger, PluginManagerType, CancellationToken> PreLaunchAction => new Action<ILogger, PluginManagerType, CancellationToken>((ILogger logger, PluginManagerType type, CancellationToken cancelToken) =>
+        {
+            string frostyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            DirectoryInfo di = new DirectoryInfo($"{frostyDir}\\Plugins\\UserPrograms");
+            if (!di.Exists) di.Create();
+
+            if (Config.Get("UserProgramLaunchingEnabled", false, ConfigScope.Game) && di.GetFiles("*.exe") != null) {
+                foreach (var file in di.GetFiles("*.exe")) {
+                    FrostyModExecutor.ExecuteProcess(file.FullName, "");
+
+                    logger.Log($"Waiting for {file.Name}");
+
+                    try {
+                        WaitForProcess(Path.GetFileNameWithoutExtension(file.Name), cancelToken);
+                    }
+                    catch (OperationCanceledException) {
+                    }
+                }
+            }
+        });
+
+        public override Action<ILogger, PluginManagerType, CancellationToken> PostLaunchAction => new Action<ILogger, PluginManagerType, CancellationToken>((ILogger logger, PluginManagerType type, CancellationToken cancelToken) =>
+        {
+            string frostyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            DirectoryInfo di = new DirectoryInfo($"{frostyDir}\\Plugins\\UserPrograms");
+            if (!di.Exists) di.Create();
+
+            if (Config.Get("UserProgramLaunchingEnabled", false, ConfigScope.Game) && di.GetFiles("*.exe") != null) {
+                logger.Log("Waiting for game");
+
+                // find game process to check when it closes
+                try {
+                    Process gameProcess = WaitForProcess(ProfilesLibrary.ProfileName, cancelToken);
+                    if (gameProcess != null) {
+                        gameProcess.EnableRaisingEvents = true;
+                        gameProcess.Exited += OnGameProcessExited;
+                    }
+                }
+                catch (OperationCanceledException) {
+                }
+            }
+        });
+
+        private void OnGameProcessExited(object sender, EventArgs e)
+        {
+            string frostyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            DirectoryInfo di = new DirectoryInfo($"{frostyDir}\\Plugins\\UserPrograms");
+            if (!di.Exists) di.Create();
+            foreach (var file in di.GetFiles("*.exe")) {
+                KillProcess(Path.GetFileNameWithoutExtension(file.Name));
+            }
+            
+        }
+
+        private Process GetProcessByName(string name)
+        {
+            Process[] processes = Process.GetProcessesByName(name);
+            if (processes.Length > 0)
+                return processes[0];
+            else
+                return null;
+        }
+
+        private Process WaitForProcess(string name, CancellationToken cancelToken)
+        {
+            Process gameProcess = null;
+            while (true)
+            {
+                cancelToken.ThrowIfCancellationRequested();
+
+                foreach (var process in Process.GetProcesses())
+                {
+                    try
+                    {
+                        cancelToken.ThrowIfCancellationRequested();
+
+                        string processFilename = process.MainModule?.ModuleName;
+                        if (string.IsNullOrEmpty(processFilename))
+                            continue;
+
+                        FileInfo fi = new FileInfo(processFilename);
+                        if (fi.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0)
+                        {
+                            gameProcess = process;
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                if (gameProcess != null)
+                {
+                    while (gameProcess.MainWindowHandle == IntPtr.Zero)
+                        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                    return gameProcess;
+                }
+            }
+        }
+
+        private void KillProcess(string process)
+        {
+            Process Processes = GetProcessByName(process);
+            if (Processes != null)
+                Processes.Kill();
+        }
+    }
+}
---
+++ Plugins/LaunchUserProgramsPlugin/Options/LaunchOptions.cs
@@ -0,0 +1,56 @@
+﻿using Frosty.Core;
+using Frosty.Core.Controls.Editors;
+using Frosty.Core.Misc;
+using FrostySdk.Attributes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchUserProgramsPlugin.Options
+{
+
+    [IsExpandedByDefault]
+    [EbxClassMeta(FrostySdk.IO.EbxFieldType.Struct)]
+    public class Programs {
+        [EbxFieldMeta(FrostySdk.IO.EbxFieldType.CString)]
+        [IsReadOnly]
+        public string Name { get; set; }
+    }
+
+    [DisplayName("Launch User Programs")]
+    public class LaunchOptions : OptionsExtension
+    {
+        [Category("General")]
+        [Description("")]
+        [DisplayName("Enabled")]
+        [EbxFieldMeta(FrostySdk.IO.EbxFieldType.Boolean)]
+        public bool UserProgramLaunchingEnabled { get; set; } = true;
+
+        [Category("General")]
+        [Description("")]
+        [EbxFieldMeta(FrostySdk.IO.EbxFieldType.Struct)]
+        [DisplayName("Programs")]
+        [IsReadOnly]
+        public List<Programs> UserPrograms { get; set; } = new List<Programs>();
+
+        public override void Load()
+        {
+            string frostyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+            DirectoryInfo di = new DirectoryInfo($"{frostyDir}\\Plugins\\UserPrograms");
+            if (!di.Exists) di.Create();
+            foreach (var file in di.GetFiles("*.exe")) {
+                UserPrograms.Add(new Programs() { Name = file.Name });
+            }
+
+            UserProgramLaunchingEnabled = Config.Get("UserProgramLaunchingEnabled", false, ConfigScope.Game);
+        }
+
+        public override void Save()
+        {
+            Config.Add("UserProgramLaunchingEnabled", UserProgramLaunchingEnabled, ConfigScope.Game);
+        }
+    }
+}
