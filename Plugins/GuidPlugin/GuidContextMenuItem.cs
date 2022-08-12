8851f1b9765b0e74ba74b463c7f964b861488374
Dyvine57 <dulana57@gmail.com>
2022-08-11 21:10:42 -0400

    initial for plugin

---
+++ Plugins/GuidPlugin/GuidContextMenuItem.cs
@@ -0,0 +1,39 @@
+﻿using Frosty.Core;
+using FrostySdk.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GuidPlugin
+{
+    public class CopyGuidContextMenuItem : DataExplorerContextMenuExtension
+    {
+
+        public override string ContextItemName => "Copy GUID";
+
+        public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/FrostyCore;component/Images/Copy.png") as ImageSource;
+
+        public override RelayCommand ContextItemClicked => new RelayCommand((o) =>
+        {
+            EbxAsset asset = App.AssetManager.GetEbx(App.SelectedAsset);
+            Clipboard.SetText(asset.RootInstanceGuid.ToString());
+        });
+    }
+
+    public class LogGuidContextMenuItem : DataExplorerContextMenuExtension
+    {
+
+        public override string ContextItemName => "Log GUIDs";
+
+        public override ImageSource Icon => new ImageSourceConverter().ConvertFromString("pack://application:,,,/FrostyEditor;component/Images/Properties.png") as ImageSource;
+
+        public override RelayCommand ContextItemClicked => new RelayCommand((o) =>
+        {
+            EbxAsset asset = App.AssetManager.GetEbx(App.SelectedAsset);
+
+            App.Logger.Log($"File GUID: {asset.FileGuid}");
+            App.Logger.Log($"Root GUID: {asset.RootInstanceGuid}");
+        });
+    }
+
+    
+}
